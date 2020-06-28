3683ea55b2396113c03326c23008757a3c0ad3b7
Quentin McGaw <quentin.mcgaw@gmail.com>
2020-06-27 21:34:51 -0400

    Set link to caddy-ui for splash log

--- internal/constants/splash.go
+++ internal/constants/splash.go
@@ -9,5 +9,5 @@ const (
 
 const (
 	// IssueLink is the link for users to use to create issues
-	IssueLink = "https://github.com/qdm12/caddy-ui-server/issues/new"
+	IssueLink = "https://github.com/qdm12/caddy-ui/issues/new"
 )
