801cc441f7fee40af01d0cc136b96a8613c6d70e
Quentin McGaw <quentin.mcgaw@gmail.com>
2020-06-28 12:55:03 -0400

    Forward error from Caddy

--- internal/processor/caddyconfig.go
+++ internal/processor/caddyconfig.go
@@ -2,8 +2,11 @@ package processor
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/qdm12/golibs/errors"
 )
 
 func (p *processor) GetCaddyConfig() (jsonContent []byte, err error) {
@@ -37,7 +40,17 @@ func (p *processor) SetCaddyConfig(jsonContent []byte) (err error) {
 	if status == http.StatusOK {
 		return nil
 	}
-	return fmt.Errorf("HTTP status code %d", status)
+	response := struct {
+		Error string `json:"error"`
+	}{}
+	if err := json.Unmarshal(jsonContent, &response); err != nil {
+		return err
 	}
-	return nil
+	switch status {
+	case http.StatusBadRequest:
+		err = errors.NewBadRequest(response.Error)
+	case http.StatusConflict:
+		err = errors.NewConflict(response.Error)
+	}
+	return err
 }
